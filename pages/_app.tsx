b4bdb2c9e11f8661823eeb6d00a1db2d5fce18db
tokatu4561 <ryobsk4561@gmail.com>
2022-11-05 19:39:39 +0900

    add cogunit auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify'
+import { Authenticator } from '@aws-amplify/ui-react';
+import awsconfig from '../src/aws-exports';
+
+Amplify.configure(awsconfig)
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Authenticator signUpAttributes={['email']}>
+      <Component {...pageProps} />
+    </Authenticator>
+  )
 }
 
 export default MyApp
