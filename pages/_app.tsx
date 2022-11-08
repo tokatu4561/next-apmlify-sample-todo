89ec7ea2526fe16d3e0718492be28066c85d60d3
tokatu4561 <ryobsk4561@gmail.com>
2022-11-08 21:17:34 +0900

    tailwindを読み込む

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import '@aws-amplify/ui-react/styles.css';
 import { Amplify } from 'aws-amplify'
 import { Authenticator } from '@aws-amplify/ui-react';
+import '../styles/globals.css'
 import awsconfig from '../src/aws-exports';
 
 Amplify.configure(awsconfig)
