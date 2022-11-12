dba695c40a03866069fc759f169ce4314e76d66c
tokatu4561 <ryobsk4561@gmail.com>
2022-11-12 23:52:37 +0900

    homeページ 各ページへのリンク

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,20 @@ import { MainLayout } from '../src/components/Layout/MainLayout'
 const Home: NextPage = () => {
   return (
     <MainLayout title="ホーム">
-      <Link href={'/isr'}>
-        <a>isr動作確認ページへ</a>
-      </Link>
+      <main className="flex justify-center items-center text-gray-600 h-screen">
+        <div className="flex flex-col space-y-8">
+          <Link href={'/isr'}>
+            <a className="text-2xl font-bold hover:underline">
+              isr動作確認ページへ
+            </a>
+          </Link>
+          <Link href={'/isr'}>
+            <a className="text-2xl font-bold hover:underline">
+              TaskListページへ（SSR）
+            </a>
+          </Link>
+        </div>
+      </main>
     </MainLayout>
   )
 }
