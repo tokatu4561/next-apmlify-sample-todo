083eee69616a19e3195a3aa94dab53f8cc6c692b
tokatu4561 <ryobsk4561@gmail.com>
2022-11-12 20:38:16 +0900

    isr page

--- pages/isr.tsx
+++ pages/isr.tsx
@@ -1,42 +1,50 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { GetStaticProps } from 'next'
 import { MainLayout } from '../src/components/Layout/MainLayout'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+import { GetStaticProps } from 'next'
 
-type Props = { timestamp: number }
+type Props = { createdAt: number; nextCreatedAt: string }
+
+dayjs.extend(timezone)
+dayjs.extend(utc)
+dayjs.tz.setDefault('Asia/Tokyo')
+
+const intervalSecond = 20
+const formatStyle = 'MM/DD HH:mm:ss'
 
 export default function ISR(props: Props) {
-  // const [authState, setAuthState] = React.useState();
-  // const [user, setUser] = React.useState();
-
-  // React.useEffect(() => {
-  //     return onAuthUIStateChange((nextAuthState, authData) => {
-  //         setAuthState(nextAuthState);
-  //         setUser(authData)
-  //     });
-  // }, []);
-
-  // AuthState.SignedIn && user ? (
-  //   <div className="App">
-  //       <div>Hello, {user.username}</div>
-  //       <AmplifySignOut />
-  //   </div>
-
+  const { createdAt, nextCreatedAt } = props
   return (
     <MainLayout title="isr">
       <main className={styles.main}>
         <h1 className={styles.title}>ISRページ</h1>
-        <p> time: {props.timestamp}</p>
+        <h2>Interval</h2>
+        <h1>{intervalSecond}s</h1>
+        <br />
+        <h2>Page accessed time</h2>
+        <h1>{dayjs().tz().format(formatStyle)}</h1>
+        <h2>Next HTML can be generated time</h2>
+        <h1>{nextCreatedAt}</h1>
+        <h2>HTML created time</h2>
+        <h1>{createdAt}</h1>
       </main>
     </MainLayout>
   )
 }
 
-export const getStaticProps: GetStaticProps<Props> = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
+  const currentTime = dayjs().tz()
+  const createdAt = currentTime.format(formatStyle)
+  const nextCreatedAt = currentTime.add(intervalSecond, 's').format(formatStyle)
+
   return {
     props: {
-      timestamp: new Date().getTime(),
+      createdAt,
+      nextCreatedAt,
     },
-    revalidate: 5, //5秒キャッシュ
+    revalidate: intervalSecond, //5秒キャッシュ
   }
 }
